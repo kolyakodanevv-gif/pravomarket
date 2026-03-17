79e27fe7cdae4434bc28c406d9662b46eb5e853e
QWE <kolyakodanevv@example.com>
2026-03-17 15:56:10 +0500

    Первый коммит: весь сайт

---
+++ script.js
@@ -0,0 +1,8 @@
+const currentPage = window.location.pathname.split('/').pop() || 'index.html';
+const navLinks = document.querySelectorAll('.nav a');
+navLinks.forEach(link => {
+    const linkPage = link.getAttribute('href');
+    if (linkPage === currentPage) {
+        link.classList.add('active');
+    }
+});
